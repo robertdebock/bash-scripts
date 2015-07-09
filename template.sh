0d1e3c6749dc4a3a25a53de21ec4e6c401864eaf
Robert de Bock <robert@meinit.nl>
2015-07-09 14:08:56 +0200

    Create template.sh
    This is a template of a shell script that can be started from the command line and gives the user an opportunity to enter options and arguments.

---
+++ template.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Description of the function of this script.
+
+usage() {
+  echo "Usage: $0 [-o option] -a argument"
+  echo
+  echo "  -o OPTION"
+  echo "    Description of this option."
+  echo "  -a ARGUMENT"
+  echo "    Description of this argument."
+  exit 1
+}
+
+readargs() {
+  while [ "$#" -gt 0 ] ; do
+    case "$1" in
+      -o)
+        if [ "$2" ] ; then
+          option="$2"
+          shift ; shift
+        else
+          echo "Missing a value for $1."
+          echo
+          shift
+          usage
+        fi
+      ;;
+      -a)
+        if [ "$2" ] ; then
+          argument="$2"
+          shift ; shift
+        else
+          echo "Missing a value for $1."
+          echo
+          shift
+          usage
+        fi
+      ;;
+      *)
+        echo "Unknown option or argument $1."
+        echo
+        shift
+        usage
+      ;;
+    esac
+  done
+}
+
+checkargs() {
+  if [ ! "${argument}" ] ; then
+    echo "Missing argument."
+    exit 2
+  fi
+}
+
+setargs() {
+  if [ ! "${option}" ] ; then
+    option="default"
+  fi
+}
+
+main() {
+  # Enter the functional part of the script here.
+  :
+}
+
+readargs "$@"
+checkargs
+setargs
+main
